87e6857996bbadf35dbeef70b364540066e229c6
smariikaale <smarikaale82@gmail.com>
2025-08-15 11:50:27 +0545

    Add files via upload

--- basicpython.py
+++ basicpython.py
@@ -1,2 +1,48 @@
 #Python //interpreted language as it checks the code line by line and throws error in run-time
-print("Hi!! It's Me Smarika")
+#print("Hi!! It's Me Smarika")//
+
+#HLL-language that is understood by humans(C,C++//programming languages)
+#MLL-binary digits(0,1)//language that is understood by machines
+#Assembly level language-language that is between HLL & MLL used to code in microprocessors
+
+#input()- takes input from the user
+#name=input("Enter your name: ")
+#print(name)
+#num1=input("Enter a number: ")
+#print(num1)
+#singer=input("Your favorite singer:")//
+#print(singer)
+#variables:a datatype used to store values temporarily
+#we use variables to store the input values
+#python doesn't have constant keyword
+#rules for defining variables: 
+#never start the name of the variables with numbers and special characters except _
+#snake case(uses underscores to separate words) is the standard way for defining variables
+
+#for classes-pascal case
+#for functions-camel case
+#for variables-snake case
+
+#num1=int(input("Enter a number:"))
+#num2=int(input("Enter another number:"))
+#print(num1+num2)
+#typecasting-process of conversion from one datatype to another
+
+#print(int("56")) #56
+#print(float(12))#12.0
+#print(str(56))#56
+#print(int("Smarika"))#error
+#print(int(5.25))#5
+
+#task2:
+# Interger:
+age=int(input("Enter your age:"))
+print(age)
+# String:
+name=input("Enter your name:")
+print(name)
+# Float
+height=float(input("Enter your height: "))
+print(height)
+
+
